3298b76bbb9eab468b62f9915c282208b9965b2b
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-07-21 17:20:52 +0200

    Add Prefabs. Finalize Map Layout. Add Smooth Cam Follow

---
+++ Assets/Scripts/SmoothCamFollow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A simple smooth camera follow script that smoothly follows the target.
+/// </summary>
+public class SmoothCamFollow : MonoBehaviour
+{
+	#region Private Variables
+	[SerializeField] private Transform target = default;
+	[SerializeField] private float smoothing = default;
+	[SerializeField] private Vector3 offset = new Vector3();
+
+	private Vector3 desiredPos = new Vector3();
+	private Vector3 smoothedPos = new Vector3();
+	#endregion
+
+	#region Private Voids
+	private void Update()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+		if(target)
+		{
+			desiredPos = new Vector3(target.position.x + offset.x, target.position.y + offset.y, target.position.z + offset.z);
+			smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothing * Time.fixedDeltaTime);
+			transform.position = smoothedPos;
+		}
+	}
+	#endregion
+}
