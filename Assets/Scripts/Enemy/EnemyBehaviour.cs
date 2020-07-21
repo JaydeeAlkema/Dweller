bf91540cbda78481b83e1160e745b452974024ca
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-07-21 13:26:58 +0200

    Add Enemy Behaviour Class

---
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The enemy behaviour class is responsible for the enemy behaviour...
+/// It handles target detection aswell as navigation towards it.
+/// </summary>
+public class EnemyBehaviour : MonoBehaviour
+{
+	#region Private Variables
+	[Header("Movement Stats")]
+	[SerializeField] private float movementSpeed = 2.5f;				// The enemy can't run, And the general movement speed is lower than the player.
+	[SerializeField] private float movementDestinationInterval = 0.25f;	// How many seconds inbetween target destination is set. Better performance.	
+	#endregion
+}
