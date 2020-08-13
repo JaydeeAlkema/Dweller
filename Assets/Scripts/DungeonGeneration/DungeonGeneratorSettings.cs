9042d05f11a7401af327c6b5f6e81d1137df665e
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-08-13 12:31:43 +0200

    Fix Dungeon Generation Settings not being set correctly.

--- Assets/Scripts/DungeonGeneration/DungeonGeneratorSettings.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGeneratorSettings.cs
@@ -8,51 +8,57 @@ using UnityEngine.SceneManagement;
 public class DungeonGeneratorSettings : MonoBehaviour
 {
 	#region Private Variables
-	private static DungeonGeneratorSettings instance = null;
-
 	[Header("Private Variables")]
-	[SerializeField] private string seed = "";                          // Seed of the dungeon
-	[SerializeField] private int minPathwayCount = 15;                  // Minimum amount of pathways that will be generated.
-	[SerializeField] private int maxPathwayCount = 30;                  // Maximum amount of pathways that will be generated.
-	[SerializeField] private int minPathwayLength = 10;                 // Minimum length of the pathway before making a turn.
-	[SerializeField] private int maxPathwayLength = 20;                 // Maximum length of the pathway before making a turn.
-	[SerializeField] private int minRoomSize = 14;                      // Min Room size.
-	[SerializeField] private int maxRoomSize = 26;                      // Max Room size.
+	private string seed = "";                          // Seed of the dungeon
+	private int minPathwayCount = 15;                  // Minimum amount of pathways that will be generated.
+	private int maxPathwayCount = 30;                  // Maximum amount of pathways that will be generated.
+	private int minPathwayLength = 10;                 // Minimum length of the pathway before making a turn.
+	private int maxPathwayLength = 20;                 // Maximum length of the pathway before making a turn.
+	private int minRoomSize = 14;                      // Min Room size.
+	private int maxRoomSize = 26;                      // Max Room size.
 
 	[Header("UI Elements")]
 	[SerializeField] private TMP_InputField seedInputField = default;   // Seed Inputfield Reference.
-	[SerializeField] private Slider minPathwayCountSlider = default;    // Min Pathway Count Slider Reference.
-	[SerializeField] private Slider maxPathwayCountSlider = default;    // Max Pathway Count Slider Reference.
-	[SerializeField] private Slider minPathwayLengthSlider = default;   // Min Pathway Length Slider Reference.
-	[SerializeField] private Slider maxPathwayLengthSlider = default;   // Max Pathway Length Slider Reference.
-	[SerializeField] private Slider minRoomsizeSlider = default;        // Min Room Size Slider Reference.
-	[SerializeField] private Slider maxRoomsizeSlider = default;        // Max Room Size Slider Reference.
-	#endregion
+	[Space]
 
-	#region Public Properties
-	public string Seed { get => seed; set => seed = value; }
-	public int MinPathwayCount { get => minPathwayCount; set => minPathwayCount = value; }
-	public int MaxPathwayCount { get => maxPathwayCount; set => maxPathwayCount = value; }
-	public int MinPathwayLength { get => minPathwayLength; set => minPathwayLength = value; }
-	public int MaxPathwayLength { get => maxPathwayLength; set => maxPathwayLength = value; }
-	public int MinRoomSize { get => minRoomSize; set => minRoomSize = value; }
-	public int MaxRoomSize { get => maxRoomSize; set => maxRoomSize = value; }
+	[SerializeField] private Slider minPathwayCountSlider = default;    // Min Pathway Count Slider Reference.
+	[SerializeField] private TextMeshProUGUI minPathwayCountText = default;
+	[Space]
+
+	[SerializeField] private Slider maxPathwayCountSlider = default;    // Max Pathway Count Slider Reference.
+	[SerializeField] private TextMeshProUGUI maxPathwayCountText = default;
+	[Space]
+
+	[SerializeField] private Slider minPathwayLengthSlider = default;   // Min Pathway Length Slider Reference.
+	[SerializeField] private TextMeshProUGUI minPathwayLengthText = default;
+	[Space]
+
+	[SerializeField] private Slider maxPathwayLengthSlider = default;   // Max Pathway Length Slider Reference.
+	[SerializeField] private TextMeshProUGUI maxPathwayLengthText = default;
+	[Space]
+
+	[SerializeField] private Slider minRoomsizeSlider = default;        // Min Room Size Slider Reference.
+	[SerializeField] private TextMeshProUGUI minRoomSizeText = default;
+	[Space]
+
+	[SerializeField] private Slider maxRoomsizeSlider = default;        // Max Room Size Slider Reference.
+	[SerializeField] private TextMeshProUGUI maxRoomSizeText = default;
+
 	#endregion
 
 	#region Monobehaviour Callback
-	private void Awake()
-	{
-		if(!instance || instance != this)
-			instance = this;
-	}
-
 	private void Start()
 	{
 		seed = seedInputField.text;
-		minPathwayCount = (int)minPathwayCountSlider.value;
-		maxPathwayCount = (int)maxPathwayCountSlider.value;
-		minPathwayLength = (int)minPathwayLengthSlider.value;
-		maxPathwayLength = (int)maxPathwayLengthSlider.value;
+
+		OnPathwayCountMinSliderValueChanged();
+		OnPathwayCountMaxSliderValueChanged();
+
+		OnPathwayLengthMinSliderValueChanged();
+		OnPathwayLengthMaxSliderValueChanged();
+
+		OnMinRoomSizeSliderValueChanged();
+		OnMaxRoomSizeSliderValueChanged();
 	}
 	#endregion
 
@@ -60,44 +66,50 @@ public class DungeonGeneratorSettings : MonoBehaviour
 	public void OnPathwayCountMinSliderValueChanged()
 	{
 		minPathwayCount = (int)minPathwayCountSlider.value;
+		minPathwayCountText.text = "Minimum: " + minPathwayCount;
 	}
 	public void OnPathwayCountMaxSliderValueChanged()
 	{
 		maxPathwayCount = (int)maxPathwayCountSlider.value;
+		maxPathwayCountText.text = "Maximum: " + maxPathwayCount;
 	}
 
 	public void OnPathwayLengthMinSliderValueChanged()
 	{
 		minPathwayLength = (int)minPathwayLengthSlider.value;
+		minPathwayLengthText.text = "Minimum: " + minPathwayLength;
 	}
 	public void OnPathwayLengthMaxSliderValueChanged()
 	{
 		maxPathwayLength = (int)maxPathwayLengthSlider.value;
+		maxPathwayLengthText.text = "Maximum: " + maxPathwayLength;
 	}
 
 	public void OnMinRoomSizeSliderValueChanged()
 	{
 		minRoomSize = (int)minRoomsizeSlider.value;
+		minRoomSizeText.text = "Minimum: " + minRoomSize;
 	}
 	public void OnMaxRoomSizeSliderValueChanged()
 	{
 		maxRoomSize = (int)maxRoomsizeSlider.value;
+		maxRoomSizeText.text = "Maximum: " + maxRoomSize;
 	}
 	#endregion
 
 	#region On Button Clicks
 	public void PlayButton_OnClick()
 	{
-		PlayerPrefs.SetString("Seed", Seed);
+		PlayerPrefs.SetString("Seed", seedInputField.text);
 
-		PlayerPrefs.SetInt("MinPathwayCount", MinPathwayCount);
-		PlayerPrefs.SetInt("MaxPathwayCount", MaxPathwayCount);
+		PlayerPrefs.SetInt("MinPathwayCount", minPathwayCount);
+		PlayerPrefs.SetInt("MaxPathwayCount", maxPathwayCount);
 
-		PlayerPrefs.SetInt("MinPathwayLength", MinPathwayLength);
-		PlayerPrefs.SetInt("MaxPathwayLength", MaxPathwayLength);
+		PlayerPrefs.SetInt("MinPathwayLength", minPathwayLength);
+		PlayerPrefs.SetInt("MaxPathwayLength", maxPathwayLength);
 
-		PlayerPrefs.SetInt("MinRoomSize", MinRoomSize);
-		PlayerPrefs.SetInt("MaxRoomSize", MaxRoomSize);
+		PlayerPrefs.SetInt("MinRoomSize", minRoomSize);
+		PlayerPrefs.SetInt("MaxRoomSize", maxRoomSize);
 
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
